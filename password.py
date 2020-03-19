bdd30f266e8349adbae759e9260bab0e0f1e6e69
Dennis Liu <dennis1224z@gmail.com>
2020-03-19 21:43:50 +0800

    basic version

---
+++ password.py
@@ -0,0 +1,17 @@
+#密碼重試程式
+#最多重試3次
+password = 'a123456'
+i = 3
+while True:
+	p = input('請輸入密碼:')
+	if p == password:
+		print('登入成功')
+		break
+	else:
+		i = i - 1
+		if i > 0:
+			print('密碼錯誤,還有', i, '次機會')
+		else:
+			print('game over')
+			break
+
