125e1150baa6bf989e4748a7b1a1e787c229b628
Dennis Liu <dennis1224z@gmail.com>
2020-03-31 00:27:37 +0800

    add read file

---
+++ products.py
@@ -0,0 +1,27 @@
+#讀取檔案
+products = []
+with open('products.csv', 'r', encoding='utf-8' ) as f:
+	for line in f:
+		if '商品名稱,商品價格' in line:
+			continue #繼續
+		[name, price] = line.strip().split(',')
+		products.append([name, price])
+print(products)	
+	
+#讓使用者輸入
+while True:
+	name = input('請輸入商品名稱: ')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格: ')
+	price = int(price)
+	products.append([name, price])
+print(products)
+#印出所有商品和價格
+for d in products:
+	print(d[0],'的價格為', d[1], '元')
+#寫入檔案
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品名稱,商品價格\n')
+	for d in products:
+		f.write(d[0] + ',' + str(d[1]) + '\n')
